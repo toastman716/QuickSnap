eae8316353bac3a590a290757f12f8cfcdcf9bc4
7678630 <7678630@student.swin.edu.au>
2016-08-12 13:22:49 +1000

    add chunkfive font

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -10,7 +10,8 @@ namespace CardGames
         {
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);      // set the cells in the bitmap to match the cards
+            SwinGame.BitmapSetCellDetails (cards, 167, 250, 13, 5, 53);
+			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 24);// set the cells in the bitmap to match the cards
         }
 
 		/// <summary>
@@ -56,14 +57,15 @@ namespace CardGames
 			Card top = myGame.TopCard;
 			if (top != null)
 			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.White, "GameFont", 0, 20);
+				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0),  Color.White, "GameFont", 0, 30);
+				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1),  Color.White, "GameFont", 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
+
 			}
 			else
 			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
+				SwinGame.DrawText ("No card played yet...",  Color.White, "GameFont", 0, 20);
 			}
 
 			// Draw the back of the cards... to represent the deck
